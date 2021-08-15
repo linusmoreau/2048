918df5dc57d48a356b4c9cffbfe6bb9af075b941
Linus Moreau <linus.moreau@gmail.com>
2021-08-14 17:43:28 -0700

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[][] grid = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j]);
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+    }
+}
